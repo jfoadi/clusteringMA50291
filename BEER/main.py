0c63a600e362f5a20023b935bb034375035d9dcf
Adam Young <adam.young.com@gmail.com>
2024-10-31 13:51:52 +0000

    Created functions that produced kmeans plots

--- BEER/main.py
+++ BEER/main.py
@@ -1,21 +1,39 @@
-# Importing the packages needed
-import pandas as pd
+# Importing the python packages needed
+import pandas as pd   # type: ignore
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans 
+
+# Importing my functions from the files in the directory
+import data_chars as ch
+import getchars as gc
+import kmeans as km
 
 # Importing the data
 data = pd.read_csv('data_Borough_school.csv')
 
-# Importing and using the data characteristics function I made 
-import data_chars as ch
+# Choose highest number of clusters to go up to
+j = 5
+
 mean1,median1,var1,std1 = ch.chars(data)
 chatactaristics = [mean1, median1, var1, std1]
 # Now have the mean, median, variance and the standard deviation    
 
+
 # Printing all of the relevent statistics
-import getchars as gc
 for col in data.columns:
     gc.getchars('mean', mean1, col)     
     gc.getchars('median', median1, col) 
     gc.getchars('variance', var1, col)          
     gc.getchars('standard diviation', std1, col)
+
+
+allkmeans, allcentres, alllabels = km.mykmeans(j, data)
+
+for j in range(2,j+1):
+    plt.scatter(data['Height'], data['Weight'], c=alllabels[f'label{j}'])
+    plt.scatter(allcentres[f'centre{j}'][:,0], allcentres[f'centre{j}'][:,1], c='red')
+    plt.savefig(f'kmeans{j}.png')
+    plt.close()
+
+
